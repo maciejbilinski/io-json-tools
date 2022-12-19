291c9164ef7c03a6eddaa6cd03318552f400195e
Angelfrost <64253119+Angelfrost@users.noreply.github.com>
2022-12-19 14:26:12 +0100

    Add tests

--- src/test/java/pl/put/poznan/transformer/logic/JSONShowDIffTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/JSONShowDIffTest.java
@@ -1,5 +1,36 @@
 package pl.put.poznan.transformer.logic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 class JSONShowDIffTest extends BaseTest{
 
+    @Test
+    void testShowDiffWorking() {
+        JSONShowDIff showDIff = new JSONShowDIff();
+        String text1 = "Line 1\nLine 2";
+        String text2 = "Line 1\nDifferent line\nAdditional line";
+        Integer[] expectedDiff = {1, 2};
+        assertEquals(showDIff.compare(text1, text2), expectedDiff);
+    }
+
+    @Test
+    void testEmptyOneString() {
+        JSONShowDIff showDIff = new JSONShowDIff();
+        String text1 = "Line 1\nLine 2\nLine 3";
+        String text2 = "";
+        Integer[] expectedDiff = {0, 1, 2};
+        assertEquals(showDIff.compare(text1, text2), expectedDiff);
+    }
+
+    @Test
+    void testBothEmptyStrings() {
+        JSONShowDIff showDIff = new JSONShowDIff();
+        String text1 = "";
+        String text2 = "";
+        Integer[] expectedDiff = {};
+        assertEquals(showDIff.compare(text1, text2), expectedDiff);
+    }
 }
